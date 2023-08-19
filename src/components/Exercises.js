b8909640764b893d1c1a65d0f51dcc124d668e6f
Kanchana Walagambahu <kanchana.walagambahu@axiatadigitallabs.com>
2023-08-19 14:35:49 +0530

    home page done

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -14,6 +14,23 @@ const Exercises = ({exercises,setExercises,bodyPart}) => {
   const indexOfFirstExercise= indexOfLastExercise - exercisesPerPage;
   const currentExercises = exercises.slice(indexOfFirstExercise,indexOfLastExercise);
 
+  useEffect(()=>{
+    const fetchExercisesData = async () => {
+      let exerciseData = [];
+
+      if (bodyPart === 'all') {
+        exerciseData = await fetchData('https://exercisedb.p.rapidapi.com/exercises',exerciseOptions);
+      }else{
+        exerciseData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,exerciseOptions);
+      }
+      
+      setExercises(exerciseData);
+    }
+
+    fetchExercisesData();
+
+  },[bodyPart])
+
   const paginate = (e, value) => {
     setCurrentPage(value);
     window.scrollTo({top: 1800,behavior: 'smooth'})
